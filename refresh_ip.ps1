9574c6101c03e6b50945c6f10a9cb2e80d872eac
Justin Pumford <jpumford@gmail.com>
2019-10-21 13:52:48 -0600

    Update refresh_ip.ps1

--- refresh_ip.ps1
+++ refresh_ip.ps1
@@ -1,8 +1,11 @@
 $hostsPath = "$env:windir\System32\drivers\etc\hosts"
-$ip = (Get-NetIPAddress -InterfaceAlias "vEthernet (Default Switch)" -AddressFamily IPv4).IPAddress
+# $ip = (Get-NetIPAddress -InterfaceAlias "vEthernet (Default Switch)" -AddressFamily IPv4).IPAddress
+$ip = (Get-VM 'vm-ubuntu').NetworkAdapters[0].IPAddresses[0]
+
+# $ip = Read-Host -Prompt 'IP?'
 
 $hosts = get-content $hostsPath
-$hosts | Foreach {if ($_ -match '^([\d | .]*)\s(.*\.dev.workfront.tech)$') 
+$hosts | ForEach-Object {if ($_ -match '^([\d | .]*)\s(.*\.dev.workfront.tech)$') 
                   {$ip + " " + $matches[2]} else {$_}} |
          Out-File $hostsPath -enc ascii
 
